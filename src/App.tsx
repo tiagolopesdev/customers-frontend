fa66c0155bbc2e9a0d8383ebd9f4cefedd433580
Tiago Lopes <saxtiago14@gmail.com>
2024-11-04 21:23:40 -0300

    feat: logic to insert or remove product

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './Pages/Home'
 import { Customer } from './Pages/Customer'
+import { MinimarketProvider } from './Context/minimarket'
 
 function App() {
 
-  return <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='/customer' element={<Customer />}/>
-    </Routes>    
-  </BrowserRouter>
+  return <MinimarketProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/customer' element={<Customer />} />
+      </Routes>
+    </BrowserRouter>
+  </MinimarketProvider>
 }
 
 export default App
