9a1983ea1a47fb40402f0f6a4ad7410d600cd85d
Tiago Lopes <saxtiago14@gmail.com>
2024-11-09 10:18:48 -0300

    refactor: changed referency backend url

--- src/Services/Bases/api.ts
+++ src/Services/Bases/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const customerChannel = axios.create({
-  // baseURL: 'https://customers-backend.onrender.com/'
-  baseURL: 'http://localhost:5048/'
+  baseURL: 'https://customers-backend.onrender.com/'
+  // baseURL: 'http://localhost:5048/'
 })
