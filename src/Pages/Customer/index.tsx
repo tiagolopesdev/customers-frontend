6cfdc29c27f080895ae40ac960c33cbe2d852913
Tiago Lopes <saxtiago14@gmail.com>
2024-11-22 21:16:44 -0300

    refactor: adjusted code for create or update customer

--- src/Pages/Customer/index.tsx
+++ src/Pages/Customer/index.tsx
@@ -1,9 +1,9 @@
-import { Button, TextField } from "@mui/material"
+import { Button, Skeleton, TextField } from "@mui/material"
 import { Values } from "../../Components/Values"
 import { ShoppingCard } from "../../Components/Cards/Shopping"
 import { PaymentsCard } from "../../Components/Cards/Payments"
 import { useEffect, useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { useNavigate, useSearchParams } from "react-router-dom"
 import { findByIdCustomersHandler } from "../../Handlers/GetByIdCustomer"
 import { ICustomer } from "../../Types/ICustomer"
 import { ObjectIsEquals } from "../../Utils/objectIsEqual"
@@ -14,12 +14,13 @@ import { updateCustomerHandler } from "../../Handlers/UpdateCustomer"
 
 export const Customer = () => {
 
+  const navigate = useNavigate()
   const [paramsUrl] = useSearchParams()
   const [customer, setCustomer] = useState<ICustomer>(initialStateCustomer)
   const [customerOrigin, setCustomerOrigin] = useState<ICustomer>(initialStateCustomer)
   const [loading, setLoading] = useState(true)
 
-  const findCustomer = async (customerIdParam?: string) => {
+  const findCustomer = async () => {
 
     const customerId = paramsUrl.get('identity') as string
 
@@ -30,23 +31,9 @@ export const Customer = () => {
       result.buys?.forEach((item) => { item.isEnable = false })
     }
 
-    if (customerIdParam) {
-      result = await findByIdCustomersHandler(customerIdParam) as ICustomer
-      result.buys?.forEach((item) => { item.isEnable = false })
-    }
-
     setCustomer(result)
     setCustomerOrigin(structuredClone(result))
-    setLoading(!loading)
-  }
-
-  const managerCommandCustomer = async () => {
-    if (customer.id) {
-      await updateCustomerHandler(customer)
-    } else {
-      const customerCreated = await createCustomerHandler(customer)
-      await findCustomer(customerCreated)
-    }
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -54,14 +41,7 @@ export const Customer = () => {
   }, [loading])
 
   const showComponent = (): JSX.Element | string => {
-    return !loading ? <div
-      style={{
-        display: 'flex',
-        flexDirection: "column",
-        height: '100dvh',
-        alignItems: "center"
-      }}
-    >
+    return !loading ?
       <div
         style={{
           backgroundColor: '#ffffff',
@@ -89,55 +69,77 @@ export const Customer = () => {
         </div>
         <ShoppingCard customer={customer} setCustomer={setCustomer} />
         <PaymentsCard customer={customer} setCustomer={setCustomer} />
-      </div>
-      <div
-        style={{
-          position: "sticky",
-          bottom: 0,
-          backgroundColor: '#1864BA',
-          display: "flex",
-          padding: '10px',
-          width: '100vw',
-          justifyContent: "center",
-          height: '10dvh',
-          flexShrink: 0,
-          alignItems: 'center'
-        }}
-      >
-        <Button
-          style={{ height: '7vh', margin: '0px 5px' }}
-          color="info"
-          variant="contained"
-        >
-          <Link
-            to="/"
-            style={{
-              color: '#ffffff'
-            }}
-          >Voltar</Link>
-        </Button>
-        <Button
-          style={{ height: '7vh', margin: '0px 5px' }}
-          color="success"
-          disabled={ObjectIsEquals(customer, customerOrigin)}
-          onClick={async () => {
-            await managerCommandCustomer()
-            setLoading(true)
+      </div> :
+      <div style={{ padding: '20px' }}>
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: '95vw',
+            height: '84dvh',
+            borderRadius: '10px',
           }}
-          variant="contained"
-        >Salvar</Button>
-        <Button
-          style={{ height: '7vh', margin: '0px 5px' }}
-          color="warning"
-          variant="contained"
-          disabled={ObjectIsEquals(customer, customerOrigin)}
-          onClick={() => { setLoading(true) }}
-        >Reverter</Button>
+        />
       </div>
-    </div> : ''
   }
 
   useEffect(() => { showComponent() }, [customer.buys, customer.payments])
 
-  return showComponent()
+  return <div
+    style={{
+      display: 'flex',
+      flexDirection: "column",
+      height: '100dvh',
+      alignItems: "center"
+    }}
+  >
+    {showComponent()}
+    <div
+      style={{
+        position: "sticky",
+        bottom: 0,
+        backgroundColor: '#1864BA',
+        display: "flex",
+        padding: '10px',
+        width: '100vw',
+        justifyContent: "center",
+        height: '10dvh',
+        flexShrink: 0,
+        alignItems: 'center'
+      }}
+    >
+      <Button
+        style={{ height: '7vh', margin: '0px 5px' }}
+        color="info"
+        variant="contained"
+        onClick={() => {
+          setCustomer(initialStateCustomer)
+          navigate("/")
+        }}
+      >
+        Voltar
+      </Button>
+      <Button
+        style={{ height: '7vh', margin: '0px 5px' }}
+        color="success"
+        disabled={ObjectIsEquals(customer, customerOrigin)}
+        onClick={async () => {
+          if (customer.id) {
+            await updateCustomerHandler(customer)
+            setLoading(true)
+          } else {
+            const customerId = await createCustomerHandler(customer)
+            window.location.assign(`${window.location.protocol}//${window.location.host}/customer?identity=${customerId}`)
+          }
+        }}
+        variant="contained"
+      >Salvar</Button>
+      <Button
+        style={{ height: '7vh', margin: '0px 5px' }}
+        color="warning"
+        variant="contained"
+        disabled={ObjectIsEquals(customer, customerOrigin)}
+        onClick={() => { setLoading(true) }}
+      >Reverter</Button>
+    </div>
+  </div>
 }
