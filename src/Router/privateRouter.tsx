b3a36dc2b88db7bb70d17fe3034b4028e9e372d6
Tiago Lopes <saxtiago14@gmail.com>
2024-12-03 12:12:58 -0300

    refactor: using private router

--- src/Router/privateRouter.tsx
+++ src/Router/privateRouter.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext } from "react"
 import { Login } from "../Pages/Login"
 import { MinimarketContext } from "../Context/minimarket"
 
@@ -12,13 +12,13 @@ export const PrivateRouter = ({ permitedElement, redirect }: IPrivateRouter) => 
 
   const { loadUserLocalStorage } = useContext(MinimarketContext)
 
-  const [isValidToken, setIsValidToken] = useState(false)
+  // const [isValidToken, setIsValidToken] = useState(false)
 
   const validationToken = (nbf: number, exp: number) => {
     const currentTime = Math.floor(Date.now() / 1000)
-    if (currentTime < nbf) setIsValidToken(false)
-    if (currentTime > exp) setIsValidToken(false)
-    return setIsValidToken(true)
+    if (currentTime < nbf) return false
+    if (currentTime > exp) return false
+    return true
   }
 
   const managerAccess = () => {
@@ -31,30 +31,19 @@ export const PrivateRouter = ({ permitedElement, redirect }: IPrivateRouter) => 
 
     const user = JSON.parse(userLocalStorage)
 
-    validationToken(user.nbf, user.exp)
+    // validationToken(user.nbf, user.exp)
 
-    // console.log('User converted ', user)
+    console.log('User converted ', user)
 
-    // if (!validationToken(user.nbf, user.exp)) {
-    //   console.log('Not validated: ', user)
-    //   return <Login toRedirect={redirect} />
-    // } else {
-    //   console.log('Validated: ', user)
-    //   loadUserLocalStorage()
-    //   return permitedElement
-    // }
-  }
-
-  const showElement = () => {    
-    if (isValidToken) {
+    if (!validationToken(user.nbf, user.exp)) {
+      console.log('Not validated: ', user)
       return <Login toRedirect={redirect} />
     } else {
+      console.log('Validated: ', user)
       loadUserLocalStorage()
       return permitedElement
     }
   }
 
-  useEffect(() => { managerAccess() }, [isValidToken])
-
-  return showElement()
+  return managerAccess()
 }
