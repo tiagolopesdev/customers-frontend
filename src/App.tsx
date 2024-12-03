ecfb24d97f0894294d80bffd1d507a04f6fa196d
Tiago Lopes <saxtiago14@gmail.com>
2024-12-02 23:09:58 -0300

    refactor: trying not use private router component

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,40 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './Pages/Home'
 import { Customer } from './Pages/Customer'
-import { MinimarketProvider } from './Context/minimarket'
+import { MinimarketContext, MinimarketProvider } from './Context/minimarket'
 import { Login } from './Pages/Login'
 import { PrivateRouter } from './Router/privateRouter'
+import { useContext } from 'react'
+import { validationToken } from './Utils/validationToken'
 
 function App() {
+
+  const { loadUserLocalStorage } = useContext(MinimarketContext)
+
+  const managerAccess = (
+    permitedElement: JSX.Element,
+    redirect?: string
+  ) => {
+
+    const userLocalStorage = localStorage.getItem('user')
+
+    console.log('UserLocal: ', userLocalStorage)
+
+    if (userLocalStorage === null) return <Login toRedirect={redirect} />
+
+    const user = JSON.parse(userLocalStorage)
+
+    console.log('User converted ', user)
+
+    if (!validationToken(user.nbf, user.exp)) {
+      console.log('Not validated: ', user)
+      return <Login toRedirect={redirect} />
+    } else {
+      console.log('Validated: ', user)
+      loadUserLocalStorage()
+      return permitedElement
+    }
+  }
 
   return <MinimarketProvider>
     <BrowserRouter>
@@ -15,19 +44,21 @@ function App() {
         <Route
           path='/'
           element={
-            <PrivateRouter
-              permitedElement={<Home />}
-              redirect='home'
-            />
+            managerAccess(<Home />, 'home')
+            // <PrivateRouter
+            //   permitedElement={<Home />}
+            //   redirect='home'
+            // />
           }
         />
         <Route
           path='/customer'
           element={
-            <PrivateRouter
-              permitedElement={<Customer />}
-              redirect='customer'
-            />
+            managerAccess(<Customer />, 'customer')
+            // <PrivateRouter
+            //   permitedElement={<Customer />}
+            //   redirect='customer'
+            // />
           }
         />
       </Routes>
