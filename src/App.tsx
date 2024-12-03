b62f9a234945197235df40ccf97d345d95c6cca1
Tiago Lopes <saxtiago14@gmail.com>
2024-12-02 23:26:58 -0300

    refactor: trying not use private router component

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,11 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { Home } from './Pages/Home'
 import { Customer } from './Pages/Customer'
-import { MinimarketContext, MinimarketProvider } from './Context/minimarket'
+import { MinimarketProvider } from './Context/minimarket'
 import { Login } from './Pages/Login'
-import { useContext } from 'react'
-import { validationToken } from './Utils/validationToken'
+import { PrivateRouter } from './Router/privateRouter'
 
 function App() {
-
-  const { loadUserLocalStorage } = useContext(MinimarketContext)
-
-  const managerAccess = (
-    permitedElement: JSX.Element,
-    redirect?: string
-  ) => {
-
-    const userLocalStorage = localStorage.getItem('user')
-
-    console.log('UserLocal: ', userLocalStorage)
-
-    if (userLocalStorage === null) return <Login toRedirect={redirect} />
-
-    const user = JSON.parse(userLocalStorage)
-
-    console.log('User converted ', user)
-
-    if (!validationToken(user.nbf, user.exp)) {
-      console.log('Not validated: ', user)
-      return <Login toRedirect={redirect} />
-    } else {
-      console.log('Validated: ', user)
-      loadUserLocalStorage()
-      return permitedElement
-    }
-  }
 
   return <MinimarketProvider>
     <BrowserRouter>
@@ -43,21 +15,19 @@ function App() {
         <Route
           path='/'
           element={
-            managerAccess(<Home />, 'home')
-            // <PrivateRouter
-            //   permitedElement={<Home />}
-            //   redirect='home'
-            // />
+            <PrivateRouter
+              permitedElement={<Home />}
+              redirect='home'
+            />
           }
         />
         <Route
           path='/customer'
           element={
-            managerAccess(<Customer />, 'customer')
-            // <PrivateRouter
-            //   permitedElement={<Customer />}
-            //   redirect='customer'
-            // />
+            <PrivateRouter
+              permitedElement={<Customer />}
+              redirect='customer'
+            />
           }
         />
       </Routes>
--- src/Router/privateRouter.tsx
+++ src/Router/privateRouter.tsx
@@ -1,6 +1,5 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Login } from "../Pages/Login"
-import { validationToken } from "../Utils/validationToken"
 import { MinimarketContext } from "../Context/minimarket"
 
 
@@ -13,27 +12,50 @@ export const PrivateRouter = ({ permitedElement, redirect }: IPrivateRouter) => 
 
   const { loadUserLocalStorage } = useContext(MinimarketContext)
 
+  const [isValidToken, setIsValidToken] = useState(false)
+
+  const validationToken = (nbf: number, exp: number) => {
+    const currentTime = Math.floor(Date.now() / 1000)
+    if (currentTime < nbf) setIsValidToken(false)
+    if (currentTime > exp) setIsValidToken(false)
+    return setIsValidToken(true)
+  }
+
   const managerAccess = () => {
 
     const userLocalStorage = localStorage.getItem('user')
 
     console.log('UserLocal: ', userLocalStorage)
-    
+
     if (userLocalStorage === null) return <Login toRedirect={redirect} />
-    
+
     const user = JSON.parse(userLocalStorage)
 
-    console.log('User converted ', user)
-    
-    if (!validationToken(user.nbf, user.exp)) {
-      console.log('Not validated: ', user)
+    validationToken(user.nbf, user.exp)
+
+    // console.log('User converted ', user)
+
+    // if (!validationToken(user.nbf, user.exp)) {
+    //   console.log('Not validated: ', user)
+    //   return <Login toRedirect={redirect} />
+    // } else {
+    //   console.log('Validated: ', user)
+    //   loadUserLocalStorage()
+    //   return permitedElement
+    // }
+  }
+
+  const showElement = () => {
+    managerAccess()
+    if (isValidToken) {
       return <Login toRedirect={redirect} />
     } else {
-      console.log('Validated: ', user)
       loadUserLocalStorage()
       return permitedElement
     }
   }
 
-  return managerAccess()
+  useEffect(() => { showElement() }, [isValidToken])
+
+  return showElement()
 }
